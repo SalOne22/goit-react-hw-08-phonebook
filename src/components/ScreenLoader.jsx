99ab1ba8f0cc9080203f752eeebda9a462bee23c
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-11 18:10:45 +0200

    Updated Loader

--- src/components/ScreenLoader.jsx
+++ src/components/ScreenLoader.jsx
@@ -1,3 +1,12 @@
+import { Flex, Text, Spinner } from '@chakra-ui/react';
+
 export const ScreenLoader = () => {
-  return <div>ScreenLoader</div>;
+  return (
+    <Flex h="100dvh" alignItems="center" justifyContent="center" gap={4}>
+      <Text fontSize="2xl" fontWeight={500}>
+        Loading
+      </Text>
+      <Spinner />
+    </Flex>
+  );
 };
