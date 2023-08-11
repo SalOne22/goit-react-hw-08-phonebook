a37c3ccda3a47d6bf9aa440b3efe6cba732d7ad7
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-11 17:07:51 +0200

    Added theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
 import { store } from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
+import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <ChakraProvider
+        theme={theme}
         toastOptions={{
           defaultOptions: { position: 'top-right', duration: 2500 },
         }}
