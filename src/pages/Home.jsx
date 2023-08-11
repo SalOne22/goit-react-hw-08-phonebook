7de9450441a62e0be15cb062d4c94c97ac6f60e6
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-11 17:09:37 +0200

    Added link to Get started button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Text, Button } from '@chakra-ui/react';
 import { Section } from '../components/Section';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -9,7 +10,7 @@ const Home = () => {
         <br />
         Effortlessly store, update, and access your contacts in one place.
       </Text>
-      <Button mt={6} colorScheme="green">
+      <Button mt={6} colorScheme="green" as={NavLink} to="/contacts">
         Get started
       </Button>
     </Section>
