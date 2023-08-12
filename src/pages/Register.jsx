981cb0bafcd905129db2dcd3c746dda72609d5ce
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-12 15:31:29 +0200

    Added redirects on pages

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,14 @@
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 import { RegisterForm } from '../components/RegisterForm';
 import { Section } from '../components/Section';
+import { selectIsAuthenticated } from '../redux/selectors';
 
 const Register = () => {
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+
+  if (isAuthenticated) return <Navigate to="/contacts" />;
+
   return (
     <Section title="Register">
       <RegisterForm />
